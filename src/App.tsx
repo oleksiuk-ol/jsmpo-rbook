71379670841a8753ac628eb8e1a8eff7e7f9a429
kurolek <oleksiukol@protonmail.ch>
2022-02-02 14:45:25 +0100

    feat: add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import React from "react";
-import "./App.css";
+import "App.css";
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import RecipePage from "./pages/RecipePage";
-import Header from "./layouts/Header";
-import UserProfile from "./pages/UserProfile";
+import Home from "pages/Home";
+import RecipePage from "pages/RecipePage";
+import Header from "layouts/Header";
+import Footer from "layouts/Footer";
+import UserProfile from "pages/UserProfile";
+import { Box } from "@mui/material";
 
 function App() {
   return (
-    <div>
+    <Box display="flex" flexDirection="column" height="100vh">
       <Header />
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="recipe/:id" element={<RecipePage />} />
-        <Route path="userProfile/:id" element={<UserProfile />} />
-      </Routes>
-    </div>
+      <Box height="100vh">
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="recipe/:id" element={<RecipePage />} />
+          <Route path="userProfile/:id" element={<UserProfile />} />
+        </Routes>
+      </Box>
+      <Footer />
+    </Box>
   );
 }
 
---
+++ src/layouts/Footer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Box } from "@mui/material";
+
+function Footer() {
+  return <Box padding="15px">Footer</Box>;
+}
+
+export default Footer;
